7626d3ae436ab07c3d270f313b4688db84003b78
Olha Afanasieva <afanasievaolja@gmail.com>
2020-05-06 10:27:49 +0200

    styling

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom';
 class HomePage extends Component {
     render() {
         return (
+            <div className="column align-center">
             <h1>
             <Link
                 to='/expenses'
             >Go to all expenses</Link>
             </h1>
+            </div>
         )
     }
 }
